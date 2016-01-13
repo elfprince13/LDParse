ce9f59f55617e5152377f7387c36e46d71859d3a
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2016-01-13 14:43:43 -0500

    Initial Commit

---
+++ LDParse/LDParse.hpp
@@ -0,0 +1,23 @@
+/*
+ *  LDParse.hpp
+ *  LDParse
+ *
+ *  Created by Thomas Dickerson on 1/13/16.
+ *  Copyright © 2016 StickFigure Graphic Productions. All rights reserved.
+ *
+ */
+
+#ifndef LDParse_
+#define LDParse_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class LDParse
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ LDParse/LDParsePriv.hpp
@@ -0,0 +1,19 @@
+/*
+ *  LDParsePriv.hpp
+ *  LDParse
+ *
+ *  Created by Thomas Dickerson on 1/13/16.
+ *  Copyright © 2016 StickFigure Graphic Productions. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class LDParsePriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
