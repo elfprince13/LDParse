8cac63c0b934a20be5ef3d371d49200eef673614
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2016-01-16 18:53:28 -0500

    Templated expect infrastructure in place

--- LDParse/Parse.hpp
+++ LDParse/Parse.hpp
@@ -27,9 +27,63 @@ namespace LDParse{
 		Action() : k(NoAction), v(0) {}
 	};
 	
+	
+	
+	typedef void(*FailF)(const TokenStream::const_iterator &, bool *);
+	template<typename Out> using ReadF = bool (*)(TokenStream::const_iterator &tokenIt, Out &o, FailF fail);
+	
+	template<typename ReadF, typename Out, size_t tokenLen, const char * tokenName, typename ErrHandler> class Expect{
+	private:
+		void failed(const std::string &expect, const TokenStream::const_iterator &tokenIt, bool * expectSuccess = nullptr) const {
+			failed(expect, tokenIt->textRepr(), expectSuccess);
+		}
+		void failed(const std::string &expect, const std::string &found, bool * expectSuccess = nullptr) const {
+			mErr("Expected " + expect, found, true);
+			if(expectSuccess != nullptr) *expectSuccess = false;
+		}
+		const FailF failF;
+		
+	public:
+		
+		typedef Expect<ReadF, Out, tokenLen, tokenName, ErrHandler> SelfType;
+		
+		ReadF &mRead;
+		ErrHandler &mErr;
+		Expect(ErrHandler &err, ReadF *read = nullptr) : mRead(*read), mErr(err) {
+			const SelfType *e = this;
+			failF = [](const TokenStream::const_iterator &tokenIt, bool * expectSuccess){
+				e->failed(tokenName, tokenIt, expectSuccess);
+			};
+		}
+		
+		
+		bool operator()(const TokenStream &tokens, TokenStream::const_iterator &tokenIt, Out &o){
+			bool ret = std::distance(tokenIt, tokens.end()) >= 1;
+			if(ret) {
+				ret = mRead(tokenIt, o, this);
+			} else {
+				failed(tokenName, "EOL");
+			}
+			return ret;
+		}
+	};
+	
+	template<const char* tokenName, typename ErrHandler> class Expect<void, TokenStream::const_iterator, 0, tokenName, ErrHandler> {
+		bool operator()(const TokenStream &tokens, TokenStream::const_iterator &tokenIt, TokenStream::const_iterator &compare){
+			bool ret = tokenIt == compare;
+			if(!ret) this->failed(tokenName, tokenIt);
+			return ret;
+		}
+	};
+	
 	template<typename MPDHandler, typename MetaHandler, typename IncludeHandler, typename LineHandler, typename TriangleHandler, typename QuadHandler, typename OptHandler, typename ErrHandler> class Parser
 	{
 	private:
+		constexpr static const char strEOL[] = "EOL";
+		constexpr static const char strNUM[] = "number";
+		constexpr static const char strCOL[] = "color reference";
+		
+		
 		Winding winding;
 		MPDHandler &mMPD;
 		MetaHandler &mMeta;
@@ -39,9 +93,63 @@ namespace LDParse{
 		QuadHandler &mQuad;
 		OptHandler &mOpt;
 		ErrHandler &mErr;
+		
+		bool readColor(TokenStream::const_iterator &tokenIt, ColorRef &color, FailF fail) const{
+			bool ret = true;
+			switch(tokenIt->k){
+				case HexInt:
+					color = {false, boost::get<int32_t>((tokenIt++)->v)};
+					break;
+				case DecInt:
+					color = {true, boost::get<int32_t>((tokenIt++)->v)};
+					break;
+				default:
+					fail(tokenIt, &ret);
+			}
+			return ret;
+		}
+		
+		bool readNumber(TokenStream::const_iterator &tokenIt, float &num, FailF fail) const{
+			bool ret = true;
+			switch(tokenIt->k){
+				case HexInt:
+				case DecInt:
+					num = boost::get<int32_t>((tokenIt++)->v);
+					break;
+				case Float:
+					num = boost::get<float>((tokenIt++)->v);
+					break;
+				default:
+					fail(tokenIt, &ret);
+			}
+			return ret;
+		}
+		
 	public:
 		
-		Parser(MPDHandler &mpd, MetaHandler &m, IncludeHandler &i, LineHandler &l, TriangleHandler &t, QuadHandler &q, OptHandler &o, ErrHandler &e) :  mMeta(m), mIncl(i), mLine(l), mTri(t), mQuad(q), mOpt(o), winding(CCW) {}
+		typedef Parser<MPDHandler, MetaHandler, IncludeHandler, LineHandler, TriangleHandler, QuadHandler, OptHandler, ErrHandler> SelfType;
+		
+		typedef Expect<void, TokenStream::const_iterator, 0, strEOL, ErrHandler> ExpectEOL;
+		const ExpectEOL expectEOL;
+		
+		typedef Expect<ReadF<ColorRef>, ColorRef, 1, strCOL, ErrHandler> ExpectColor;
+		const ExpectColor expectColor;
+		
+		typedef Expect<ReadF<float>, float, 1, strNUM, ErrHandler> ExpectNumber;
+		const ExpectNumber expectNumber;
+		
+		Parser(MPDHandler &mpd, MetaHandler &m, IncludeHandler &i, LineHandler &l, TriangleHandler &t, QuadHandler &q, OptHandler &o, ErrHandler &e)
+		:  mMeta(m), mIncl(i), mLine(l), mTri(t), mQuad(q), mOpt(o), winding(CCW), expectEOL(mErr) {
+			const SelfType *self = this;
+			expectColor = ExpectColor(mErr,
+									  [](TokenStream::const_iterator &tokenIt, ColorRef &color, FailF fail){
+										  self->readColor(tokenIt, color, fail);
+									  });
+			expectNumber = ExpectNumber(mErr,
+									  [](TokenStream::const_iterator &tokenIt, float &num, FailF fail){
+										  self->readNumber(tokenIt, num, fail);
+									  });
+		}
 		
 		void parseModels(const ModelStream &models){
 			std::vector<bool> completed(models.size(), false);
@@ -54,14 +162,16 @@ namespace LDParse{
 					const TokenStream &line = *lineIt;
 					if(line.size()){
 						Action nextAction;
-						switch(line[0].k){
+						auto token = line.begin();
+						switch((token++)->k){
 							case Zero:
 								break;
-							case One:
-								//ColorRef color = expectColor(line[1]);
+							case One: {
+								ColorRef color; expectColor(line, token, color);
 								
 								//nextAction = mIncl();
 								break;
+							}
 							case Two:
 							case Three:
 							case Four:
@@ -73,9 +183,7 @@ namespace LDParse{
 					}
 				}
 			}
-			
 		}
-		
 		
 		static inline std::string coalesceText(TokenStream::const_iterator start, TokenStream::const_iterator end){
 			std::string ret("");
