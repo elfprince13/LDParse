cbcd3d3792a792508a863182f54a8e9cd35edc82
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2020-07-05 14:10:37 -0400

    Output and color-code parsing

--- face-unwrapper/face-fetcher.py
+++ face-unwrapper/face-fetcher.py
@@ -9,6 +9,8 @@ import re
 import time
 import json
 
+import csv
+
 headers = {'User-Agent':"Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11"}
 encoding = re.compile(r"^Content-Type: text/html;\s*charset=(.*)$",flags=re.MULTILINE | re.IGNORECASE)
 json_encoding = re.compile(r"^Content-Type: application/json;\s*charset=(.*)$",flags=re.MULTILINE | re.IGNORECASE)
@@ -18,6 +20,7 @@ colorCode = re.compile(r"background-color: #([0-9a-fA-F]{6})")
 brickLinkID = re.compile(r"^0 !KEYWORDS .*Bricklink.+(3626[a-z0-9]+)",flags=re.MULTILINE | re.IGNORECASE)
 allKeyWords = re.compile(r"^0 !KEYWORDS (.+)",flags=re.MULTILINE | re.IGNORECASE)
 numericPrefix = re.compile(r"^([0-9]+).*")
+colorMatcher = re.compile(r"^0\s+// LEGOID\s+(\d+) - ([a-zA-Z ]+)\s*\n0 !COLOUR\s+([a-zA-Z_]+)\s+CODE\s+(\d+)",re.MULTILINE | re.IGNORECASE)
 
 def extractBestMatch(jsonSrc, keyWords, nameNum):
 	data = json.loads(jsonSrc)
@@ -119,60 +122,93 @@ def cleanLine(line):
 def txtToKeyWords(line):
 	return {word for word in cleanLine(line).lower().split(" ") if word}
 
-def main(faces):
-	for face in faces:
-		name = os.path.basename(face)
-		name = name.split(".")[0]
-		nameNum = int(numericPrefix.match(name).group(1))
-		print(name, nameNum)
-		
-		data = fetchPartPage(name)
-		if data:
-			print(extractKnownColors(data))
-		else:
+def main(faces, colortable, output, begin = 0):
+	colortable_backup = {k.replace("_"," ").lower() : v for _, (_, v, k) in colortable.items()}
+	numFaces = len(faces)
+	with open(output,'a',newline='') as out_handle:
+		writer = csv.writer(out_handle)
+		for i,face in enumerate(faces[begin:],begin):
+			print("Acquiring data for %d / %d" % ((i+1),numFaces))
+			name = os.path.basename(face)
+			name = name.split(".")[0]
+			nameNum = int(numericPrefix.match(name).group(1))
+			print(name)
+			out_line = [face, name, None, None, None, None, None, None]
+			
 			model=""
 			with open(face,'r') as model_handle:
 				model = model_handle.read()
-			maybeName = brickLinkID.search(model)
-			if maybeName:
-				name = maybeName.group(1)
-				print("Model source specifies Bricklink %s" % name)
-				data = fetchPartPage(name)
-				if data:
-					print(extractKnownColors(data))
-				else:
-					print("Try to search later")
+			if model[:11] == "0 ~Moved to":
+				print("\tSkipping defunct part %s" % name)
+				continue
+			elif "2-Sided" in model.splitlines()[0]:
+				out_line[5] = "*"
+				print("\t2-Sided")
+			data = fetchPartPage(name)
+			if data:
+				out_line[2] = name
+				out_line[3] = extractKnownColors(data)
 			else:
-				nameLine = model.splitlines()[0]
-				nameLine = nameLine.split(" ")[1:]
-				nameWords = [cleanLine(ldraw2bricklink.get(phrase.lower(),phrase))
-							for phrase in nameLine if phrase.lower() not in {"pattern","/"}]
-				nameWords = [word for word in nameWords if word]
-				nameLine = " ".join(nameWords)
-				if len(nameLine) > 75:
-					cutAt = nameLine.rfind(" ",0,75)
-					nameLine = nameLine[:cutAt]
-				
-				keyWords = set(nameLine.lower().split(" "))
-				maybeMoreKeyWords = allKeyWords.search(model)
-				if maybeMoreKeyWords:
-					keyWords |= txtToKeyWords(maybeMoreKeyWords.group(1))
-					
-				searchData = fetchSearchPage(nameLine)
-				if searchData:
-					bestName = extractBestMatch(searchData, keyWords, nameNum)
-					if bestName:
-						data = fetchPartPage(bestName)
-						if data:
-							print(extractKnownColors(data))
-						else:
-							print("Try to search later")
+				maybeName = brickLinkID.search(model)
+				if maybeName:
+					name = maybeName.group(1)
+					print("Model source specifies Bricklink %s" % name)
+					data = fetchPartPage(name)
+					if data:
+						out_line[2] = name
+						out_line[3] = extractKnownColors(data)
 				else:
-					print("Try to search later")
+					nameLine = model.splitlines()[0]
+					nameLine = nameLine.split(" ")[1:]
+					nameWords = [cleanLine(ldraw2bricklink.get(phrase.lower(),phrase))
+								for phrase in nameLine if phrase.lower() not in {"pattern","/"}]
+					nameWords = [word for word in nameWords if word]
+					nameLine = " ".join(nameWords)
+					if len(nameLine) > 75:
+						cutAt = nameLine.rfind(" ",0,75)
+						nameLine = nameLine[:cutAt]
+					
+					keyWords = set(nameLine.lower().split(" "))
+					maybeMoreKeyWords = allKeyWords.search(model)
+					if maybeMoreKeyWords:
+						keyWords |= txtToKeyWords(maybeMoreKeyWords.group(1))
 						
-		time.sleep(0.5)
+					searchData = fetchSearchPage(nameLine)
+					if searchData:
+						bestName = extractBestMatch(searchData, keyWords, nameNum)
+						if bestName:
+							data = fetchPartPage(bestName)
+							if data:
+								out_line[2] = bestName
+								out_line[3] = extractKnownColors(data)
+			if out_line[3]:
+				out_line[4] = []
+				for (code, name, _) in out_line[3]:
+					greyed = name.replace("Gray","Grey")
+					if greyed.lower() in colortable_backup:
+							out_line[4].append((colortable_backup[greyed.lower()],greyed.replace(" ","_")))
+					else:
+						print("No fall-back available, send help")
+						out_line[6] = "*"
+			if not out_line[2]:
+				out_line[6] = "*"
+			writer.writerow(out_line)
+			time.sleep(0.5)
+			if wait_for > 4:
+				print("back off too long, stopping after %d",i)
+				break
+			
 			
 if __name__ == '__main__':
 	import sys
+	data = []
 	with open(sys.argv[1],'r') as files_list:
-		main(files_list.read().splitlines())
+		data = files_list.read().splitlines()
+	colorTable = {}
+	with open(sys.argv[2],'r') as ldconfig:
+		colorData = ldconfig.read()
+		colorData = colorMatcher.findall(colorData)
+		colorTable = {blCode : (blName, ldCode, ldName) for (blCode, blName, ldName, ldCode) in colorData}
+	outfile = sys.argv[3]
+	start = sys.argv[4] if len(sys.argv) > 4 else 0
+	main(data, colorTable, outfile, start)
